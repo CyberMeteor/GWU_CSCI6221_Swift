fcb3d62bf5d727ae99b0ed29e207a5e32a27b24f
Robert Zhang <ryz3incorporated@gmail.com>
2024-03-20 16:50:51 -0400

    Initial Commit

---
+++ Swift-Karaoke/Swift_KaraokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_KaraokeApp.swift
+//  Swift-Karaoke
+//
+//  Created by Robert Zhang on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_KaraokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
