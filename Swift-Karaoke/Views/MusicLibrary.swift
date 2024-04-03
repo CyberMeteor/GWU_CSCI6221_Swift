d25194037b74fca824c551f85890ec1ab8248722
macos_test_tusrau <tusrau@gmail.com>
2024-04-03 00:58:22 -0400

    reset

--- Swift-Karaoke/Views/MusicLibrary.swift
+++ Swift-Karaoke/Views/MusicLibrary.swift
@@ -8,32 +8,11 @@
 import SwiftUI
 
 struct MusicLibrary: View {
-    // 假设这里是你的播放列表数据
-    let playlist = Playlist(name: "Love", songs: [
-        Song(name: "歌曲1", artist: "歌手A"),
-        Song(name: "歌曲2", artist: "歌手B"),
-        // 添加更多歌曲
-    ])
-    
     var body: some View {
-        NavigationView {
-            List(playlist.songs) { song in
-                HStack {
-                    Text(song.name)
-                        .bold()
-                    Spacer()
-                    Text(song.artist)
-                        .foregroundColor(.secondary)
-                }
-                .padding(.vertical, 4)
-            }
-            .navigationTitle(playlist.name)
-        }
+        Text("Music Library")
     }
 }
 
-struct MusicLibrary_Previews: PreviewProvider {
-    static var previews: some View {
-        MusicLibrary()
-    }
+#Preview {
+    MusicLibrary()
 }
