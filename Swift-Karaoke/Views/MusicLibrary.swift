4ad7c22e4ddae7c4b61ac050432d3baa79c3407f
Kunye0309 <kunyetang@gmail.com>
2024-04-07 21:30:07 -0400

    add comments to MusicLibrary

--- Swift-Karaoke/Views/MusicLibrary.swift
+++ Swift-Karaoke/Views/MusicLibrary.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct Song: Hashable {
-    var id = UUID()
     var name: String
     var artist: String
     var image: String
@@ -22,10 +21,19 @@ struct MusicLibrary: View {
                  Song(name: "song 3", artist: "artist 3", image: "2", audioFilename: "Tuesday", time: "2:36"),
                  Song(name: "song 4", artist: "artist 4", image: "1", audioFilename: "Tuesday", time: "2:36")]
     
+    // 控制'MusicPlayer()'视图展开
     @Binding var expandSheet: Bool
+    
+    // 'Namespace' 用于创建一个命名空间，允许在视图间匹配动画，
+    // 每个命名空间唯一，通过它的 ID 标识，
+    // 进行视图转换时，指定相同的命名空间创建过渡动画
     var animation: Namespace.ID
     
+    // 点'play'按钮后，'currentSong'接收对应的'Song'的实例 （253行）
     @State private var currentSong: Song?
+    
+    // 点'play'按钮后，更新'showMusicPlayer'的值为'true' （254行）
+    // 'showMusicPlayer'被赋值给 'expandSheet'; 'expandSheet'作为参数传递给 'MusicPlayer()' （211行）
     @State private var showMusicPlayer = false
     
     var body: some View {
@@ -47,11 +55,11 @@ struct MusicLibrary: View {
                         .padding(.top, 5)
                     
                     ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 17) {
-                            HStack {
-                                ForEach(self.songs, id: \.self) { song in
-                                    RecentPlayed(song: song, currentSong: $currentSong, showMusicPlayer: $showMusicPlayer)
-                                }
+                        HStack(spacing: 17)  {
+                            // 'ForEach' 遍历 'songs' 列表中的元素('Song'实例)，
+                            // 为每个'Song'实例 生成一个 'RecentPlayed()' 视图 (218行)
+                            ForEach(self.songs, id: \.self) { song in
+                                RecentPlayed(song: song, currentSong: $currentSong, showMusicPlayer: $showMusicPlayer)
                             }
                         }
                         .padding([.horizontal, .bottom])
@@ -195,9 +203,11 @@ struct MusicLibrary: View {
                 }
             }
         }
-        // popup musicplayer view
+        // 从底部弹出'MusicPlayer()'视图
         .sheet(isPresented: $showMusicPlayer) {
             if let currentSong = currentSong {
+                // '.wrappedValue' 用于从 'Namespace()' 实例中获取其包装的值，即'Namespace.ID'
+                // 'Namespace.ID' 传递给 'MusicPlayer()' 实现视图到另一视图的过渡
                 MusicPlayer(expandSheet: $showMusicPlayer, animation: Namespace().wrappedValue)
             }
         }
@@ -206,6 +216,7 @@ struct MusicLibrary: View {
 
 
 struct RecentPlayed: View {
+    // 'Song'类型的变量
     var song: Song
     @Binding var currentSong: Song?
     @Binding var showMusicPlayer: Bool
@@ -238,6 +249,7 @@ struct RecentPlayed: View {
                             Spacer()
                             
                             Button {
+                                // Executes when pressing 'play'
                                 currentSong = song
                                 showMusicPlayer = true
                             } label: {
